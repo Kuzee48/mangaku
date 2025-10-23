ccd371150df89ef6e059d344aee18c65940925b2
Kuzee48 <rizqy.alistia1@sma.belajar.id>
2025-10-23 20:36:24 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Halo, Proyek Next.js</h1>
+      <p className="mt-2">File ini dibuat manual di GitHub.</p>
+    </main>
+  );
+}
